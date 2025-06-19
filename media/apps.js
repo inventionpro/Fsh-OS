e4cab6e29e144a503b16e17b6d895aaaac456468
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-06-19 16:56:53 +0200

    Files update

--- media/apps.js
+++ media/apps.js
@@ -11,30 +11,46 @@ const files = {
   <head>
     <style>
       body {
+        display: flex;
+        flex-direction: column;
         width: 100vw;
         height: 100vh;
         color: white;
         font-family: Lexend, Arial, sans-serif;
         margin: 0px;
       }
+      .h {
+        display: flex;
+        flex: 1;
+      }
+      #folders, #main {
+        display: flex;
+        flex-direction: column;
+      }
     </style>
   </head>
   <body>
     <div id="path"></div>
-    <div>
+    <div class="h">
       <div id="folders"></div>
       <div id="main"></div>
     </div>
     <script>
       const FS = window.parent.FS;
-      function traverse(path) {
-        let obj = {};
-        FS.get(path).filter(f=>!f.includes('.')).forEach(p=>{
-          obj[p] = traverse(path+'/'+p);
-        });
-        return obj;
+      let current = '/';
+      function traverse(o, n) {
+        return '<details><summary>' + n + '</summary>' + Object.keys(o).map(p=>{
+          if (!p.includes('.')) {
+            return traverse(o[p], p);
+          }
+          return '';
+        }).join('') + '</details>';
       }
-      console.log(traverse(''));
+      document.getElementById('folders').innerHTML = traverse(FS.tree, '/');
+      function showContents() {
+        document.getElementById('main').innerHTML = FS.get(current).map(f=>'<button>'+f+'</button>');
+      }
+      showContents();
     </script>
   </body>
 </html>`
