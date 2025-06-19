faf65bdc3add1621b824c6930e86450aeb777c3a
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-06-19 12:24:08 +0200

    Move apps to files

--- media/apps.js
+++ media/apps.js
@@ -1,10 +1,26 @@
-export const files = {
-    id: 'files',
-    name: 'Files',
-    icon: './media/app/files.svg',
-    html: ``
+function $(o){
+  return JSON.stringify(o, null, 2);
+}
+
+const files = {
+  id: 'files',
+  name: 'Files',
+  icon: './media/app/files.svg',
+  html: `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <style>
+      body {
+        background: red;
+      }
+    </style>
+  </head>
+  <body>
+    <p>HH</p>
+  </body>
+</html>`
 };
 
-export let default_apps = [
-    files
-];
+export let default_apps = {
+  'files.app': $(files)
+};
